c0188ff1adb30523b1f4a9d7e75a7ca547573a36
Joaquin Giraldo-Laguna <joaquing@mit.edu>
2020-05-01 02:42:35 -0400

    featured spomeniks

--- 10 spomenik/final_draft/SpomenikVisual.js
+++ 10 spomenik/final_draft/SpomenikVisual.js
@@ -3,6 +3,9 @@ function SpomenikVisual(idx) {
   var attrs = [];
   var state = [new SoftNum(0), new SoftNum(0), new SoftNum(0), new SoftNum(1)] // [x coordinate, y coordinate, rotation, image width]
 
+  var randomTilt = random(-0.05, 0.05);
+
+  var featured;
   var attrIndex = {};
 
   // store index in relevant designer keys
@@ -19,7 +22,7 @@ function SpomenikVisual(idx) {
   }
 
   // store index in relevant material keys
-  for (i in spmk.materials){
+  for (i in spmk.materials) {
     attrs.push(spmk.materials[i]);
     if (attrDictionary[spmk.materials[i]] == null) {
       attrDictionary[spmk.materials[i]] = [idx];
@@ -32,7 +35,7 @@ function SpomenikVisual(idx) {
   }
 
   // store index in relevant condition keys
-  for (i in spmk.condition){
+  for (i in spmk.condition) {
     attrs.push(spmk.condition[i]);
     if (attrDictionary[spmk.condition[i]] == null) {
       attrDictionary[spmk.condition[i]] = [idx];
@@ -56,7 +59,7 @@ function SpomenikVisual(idx) {
     translate(state[0].value, state[1].value)
     rotate(state[2].value)
     imageMode(CENTER);
-    image(spmkImgs[idx], 0, 0, state[3].value, state[3].value*imgRatio)
+    image(spmkImgs[idx], 0, 0, state[3].value, state[3].value * imgRatio)
     pop()
     // fill(55, 55, 55, 200);
     // noStroke();
@@ -65,16 +68,34 @@ function SpomenikVisual(idx) {
   }
   this.setLayout = function(layout) {
     if (layout == 'title') {
-      state[0].setTarget(width/nRowsTitle*(idx%nRowsTitle) + width/nRowsTitle/2)
-      state[1].setTarget(width/nRowsTitle*imgRatio*floor(idx/nRowsTitle) + width/nRowsTitle*imgRatio/2)
+      let imgWidth = width / nRowsTitle
+      state[0].setTarget(imgWidth * (idx % nRowsTitle) + imgWidth / 2)
+      state[1].setTarget(imgWidth * imgRatio * floor(idx / nRowsTitle) + imgWidth * imgRatio / 2)
       state[2].setImperfectTarget(0, 0.1)
-      state[3].setImperfectTarget(width/nRowsTitle, 15)
-    }
-    else if (layout == 'story') {
-      state[0].setTarget(height/nSpomeniks*nColsStory/imgRatio*(idx%nColsStory) + height/nSpomeniks*nColsStory/imgRatio/2);
-      state[1].setTarget(height/nSpomeniks*nColsStory*floor(idx/nColsStory) + height/nSpomeniks*nColsStory/2);
+      state[3].setImperfectTarget(imgWidth, 15)
+    } else if ((layout == 'story') && !featured) {
+      let imgHeight = height / nSpomeniks * nColsStory;
+      state[0].setTarget(imgHeight / imgRatio * (idx % nColsStory) + imgHeight / imgRatio / 2);
+      state[1].setTarget(imgHeight * floor(idx / nColsStory) + imgHeight / 2);
       state[2].setTarget(0)
-      state[3].setTarget(height/nSpomeniks*nColsStory/imgRatio)
+      state[3].setTarget(imgHeight / imgRatio)
+    } else if ((layout == 'story') && featured) {
+      let sbWidth = height / nSpomeniks * nColsStory * nColsStory / imgRatio;
+      // let imgWidth = width - 2 * sbWidth;
+      let imgWidth = 5 / 8 * width;
+      state[0].setTarget((width - sbWidth) / 2 + sbWidth)
+      state[1].setTarget(2 / 3 * imgWidth * imgRatio)
+      state[2].setTarget(randomTilt)
+      state[3].setTarget(imgWidth);
+    }
+  }
+  this.checkClick = function() {
+    if (mouseInState(state, imgRatio)) {
+      console.log(spmk.title + ' was clicked!');
+      currentLayout = 'story';
+      featured = true;
+    } else {
+      featured = false;
     }
   }
 }
--- 10 spomenik/final_draft/final_draft.js
+++ 10 spomenik/final_draft/final_draft.js
@@ -8,17 +8,19 @@ var nRowsTitle = 7; // number of rows in photo matrix (15 columns)
 var nColsStory = 3; // number of columns in sidebar matrix (15 columns)
 var attrDictionary = {}; // attributes as keys with spmk index lists
 
-var imgRatio = 536.4571428571429/957.9619047619047 // image ratio for spomenik pictures
+var imgRatio = 536.4571428571429 / 957.9619047619047 // image ratio for spomenik pictures
 
 var logoState = [new SoftNum(0), new SoftNum(0), new SoftNum(0), new SoftNum(0)]
 
 var currentLayout = 'title'
 var allLayouts = ['title', 'story']
 
+// var spmkFeature;
+
 function preload() {
   spmkJSON = loadJSON("https://raw.githubusercontent.com/joaquin-gl/4.032-sketches/master/10%20spomenik/data/spomenik.json");
   yugoImg = loadImage("https://raw.githubusercontent.com/joaquin-gl/4.032-sketches/master/10%20spomenik/data/export_yugoslavia.png");
-  logoImg = loadImage("logo.png")
+  logoImg = loadImage("https://raw.githubusercontent.com/joaquin-gl/4.032-sketches/master/10%20spomenik/data/logo.png")
   // spmkJSON = loadJSON("../data/spomenik.json");
   // yugoImg = loadImage("../data/export_yugoslavia.png");
   for (let i = 0; i < nSpomeniks; i++) {
@@ -39,45 +41,69 @@ function setup() {
 function draw() {
   background(55);
 
-  // update, display each spomenik
+  // set site layout targets
   siteLayout(currentLayout);
+
+  // update logo state
   logoState[0].update();
   logoState[1].update();
   logoState[2].update();
   logoState[3].update();
+
+  // update spomenik states
   data.forEach(function(entry) {
     entry.update();
     entry.display();
   })
+
+  // move logo where it needs to be
   push();
   translate(logoState[0].value, logoState[1].value)
   rotate(logoState[2].value)
   imageMode(CENTER);
-  image(logoImg, 0, 0, logoState[3].value, logoState[3].value*1.7/13.3)
+  image(logoImg, 0, 0, logoState[3].value, logoState[3].value * 1.7 / 13.3)
   pop();
 }
 
 function siteLayout(layout) {
   if (layout == 'title') {
-    logoState[0].setTarget(width/2)
-    logoState[1].setTarget(height/4)
+    logoState[0].setTarget(width / 2)
+    logoState[1].setTarget(height / 4)
     logoState[2].setTarget(0)
-    logoState[3].setTarget(width/2)
-  }
-  else if (layout == 'story') {
-    logoState[0].setTarget(width - width/8 - 20)
-    logoState[1].setTarget(height - width/8*1.7/13.3 - 20)
+    logoState[3].setTarget(width / 2)
+  } else if (layout == 'story') {
+    logoState[0].setTarget(width - width / 8 - 20)
+    logoState[1].setTarget(height - width / 8 * 1.7 / 13.3 - 20)
     logoState[2].setTarget(0)
-    logoState[3].setTarget(width/4)
+    logoState[3].setTarget(width / 4)
   }
   data.forEach(function(entry) {
-    entry.setLayout(layout);
+    entry.setLayout(layout)
   })
 }
 
 function mousePressed() {
-  if (currentLayout == 'title') {currentLayout = 'story'}
-  else if (currentLayout == 'story') {currentLayout = 'title'};
+  // if (currentLayout == 'title') {
+  //   currentLayout = 'story'
+  // } else if (currentLayout == 'story') {
+  //   currentLayout = 'title'
+  // };
+
+  if (mouseInState(logoState, 1.7 / 13.3)) {
+    console.log('logo was clicked!');
+    currentLayout = 'title'
+  } else {
+    data.forEach(function(entry) {
+      entry.checkClick()
+    })
+  }
+}
+
+function mouseInState(objectState, objectRatio) {
+  return ((mouseX > objectState[0].value - objectState[3].value / 2) &&
+    (mouseX < objectState[0].value + objectState[3].value / 2) &&
+    (mouseY > objectState[1].value - objectState[3].value / 2 * objectRatio) &&
+    (mouseY < objectState[1].value + objectState[3].value / 2 * objectRatio))
 }
 
 function windowResized() {
